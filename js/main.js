44b6411122f0a465282113da4fe44a5d1bd8d749
Vando dos reis <vandodosreis2001@gmail.com>
2020-09-21 13:28:02 -0300

    Game play e dificuldade do game pronto

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ var altura = 0
 var largura = 0
 var vidas = 1
 var tempo = 15
+var criaMosquitoTempo = 1500
+
+
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+	criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+	criaMosquitoTempo = 1000
+} else if(nivel === 'chucknorris') {
+	criaMosquitoTempo = 750
+}
+
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
