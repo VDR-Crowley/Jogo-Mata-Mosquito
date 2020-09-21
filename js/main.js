e55f020d5517ae084a2ef4594d70f141e6eb955a
Vando dos reis <vandodosreis2001@gmail.com>
2020-09-20 23:23:03 -0300

    Mudando o tamanho do mosquito

--- js/main.js
+++ js/main.js
@@ -26,11 +26,28 @@ function posicaoRandomica() {
 	// Criando elementos html
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosca.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 
 
 	document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+
+	switch (classe) {
+		case 0:
+			return 'mosquito1'
+		
+		case 1:
+			return 'mosquito2'
+		
+		case 2:
+			return 'mosquito3'
+
+	}
 }
