ad043ac6143588ed2cad12377f1c595f0b8c75c6
Vando dos reis <vandodosreis2001@gmail.com>
2020-09-21 02:18:27 -0300

    Game play pronto, pagina game over e vitoria prontos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -11,7 +13,33 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+	tempo -= 1
+
+	if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosquito)
+		window.location.href = 'vitoria.html'
+	} else {
+		document.getElementById('cronometro').innerHTML = tempo  // inseri um texto dentro da tag
+	}
+}, 1000)
+
 function posicaoRandomica() {
+
+	//remover o mosquito anterior (caso exista)
+	var remove_mosquito = document.getElementById('mosquito')
+	// verifica se existe (true)
+	if(remove_mosquito) {
+		document.getElementById('mosquito').remove()
+		if(vidas > 3) {
+
+			window.location.href = 'fim_de_jogo.html'
+		} else {
+		document.getElementById('vida' + vidas).src="imagens/coracao_vazio.png"
+		vidas++
+		}
+	}
 	
 	// Para não passar do tamanho da tela
 	var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -26,10 +54,14 @@ function posicaoRandomica() {
 	// Criando elementos html
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosca.png'
-	mosquito.className = tamanhoAleatorio()
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() 
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
 
 
 	document.body.appendChild(mosquito)
@@ -49,5 +81,17 @@ function tamanhoAleatorio() {
 		case 2:
 			return 'mosquito3'
 
+	}
+}
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+
+	switch (classe) {
+		case 0:
+			return 'ladoA'
+		
+		case 1:
+			return 'ladoB'
 	}
 }
